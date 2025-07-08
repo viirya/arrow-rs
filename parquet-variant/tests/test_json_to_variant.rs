df7eb7729055ffe4a2975336e2d2dc2ccf965735
Liang-Chi Hsieh <liangchi.hsieh@databricks.com>
2025-07-07 23:09:37 -0700

    speed up test_json_to_variant_object_very_large

--- parquet-variant/tests/test_json_to_variant.rs
+++ parquet-variant/tests/test_json_to_variant.rs
@@ -30,10 +30,22 @@ struct JsonToVariantTest<'a> {
 
 impl JsonToVariantTest<'_> {
     fn run(self) -> Result<(), ArrowError> {
+        self.run_test(true)
+    }
+
+    fn run_without_validation(self) -> Result<(), ArrowError> {
+        self.run_test(false)
+    }
+
+    fn run_test(self, validate: bool) -> Result<(), ArrowError> {
         let mut variant_builder = VariantBuilder::new();
         json_to_variant(self.json, &mut variant_builder)?;
         let (metadata, value) = variant_builder.finish();
-        let variant = Variant::try_new(&metadata, &value)?;
+        let variant = if validate {
+            Variant::try_new(&metadata, &value)?
+        } else {
+            Variant::new(&metadata, &value)
+        };
         assert_eq!(variant, self.expected);
         Ok(())
     }
@@ -450,8 +462,7 @@ fn test_json_to_variant_object_complex() -> Result<(), ArrowError> {
     .run()
 }
 
-#[test]
-fn test_json_to_variant_object_very_large() -> Result<(), ArrowError> {
+fn get_very_large_json() -> String {
     // 256 elements (keys: 000-255) - each element is an object of 256 elements (240-495) - each
     // element a list of numbers from 0-127
     let keys: Vec<String> = (0..=255).map(|n| format!("{n:03}")).collect();
@@ -472,14 +483,19 @@ fn test_json_to_variant_object_very_large() -> Result<(), ArrowError> {
             .join(format!(":{innermost_list},").as_str()),
         innermost_list
     );
-    let json = format!(
+    format!(
         "{{{}:{}}}",
         keys.iter()
             .map(|k| format!("\"{k}\""))
             .collect::<Vec<String>>()
             .join(format!(":{inner_object},").as_str()),
         inner_object
-    );
+    )
+}
+
+#[test]
+fn test_validate_json_to_variant_object_very_large() -> Result<(), ArrowError> {
+    let json = get_very_large_json();
     // Manually verify raw JSON value size
     let mut variant_builder = VariantBuilder::new();
     json_to_variant(&json, &mut variant_builder)?;
@@ -494,29 +510,23 @@ fn test_json_to_variant_object_very_large() -> Result<(), ArrowError> {
     // Size of inner object: 1 + 4 + 256 + 257 * 3 + 256 * 516 = 133128
     // Size of json: 1 + 4 + 512 + 1028 + 256 * 133128 = 34082313
     assert_eq!(value.len(), 34082313);
+    Ok(())
+}
 
+#[test]
+fn test_json_to_variant_object_very_large() -> Result<(), ArrowError> {
+    let json = get_very_large_json();
+    // Manually verify raw JSON value size
     let mut variant_builder = VariantBuilder::new();
-    let mut object_builder = variant_builder.new_object();
-    keys.iter().for_each(|key| {
-        let mut inner_object_builder = object_builder.new_object(key);
-        inner_keys.iter().for_each(|inner_key| {
-            let mut list_builder = inner_object_builder.new_list(inner_key);
-            for i in 0..=127 {
-                list_builder.append_value(Variant::Int8(i));
-            }
-            list_builder.finish();
-        });
-        inner_object_builder.finish().unwrap();
-    });
-    object_builder.finish().unwrap();
+    json_to_variant(&json, &mut variant_builder)?;
     let (metadata, value) = variant_builder.finish();
-    let variant = Variant::try_new(&metadata, &value)?;
+    let v = parquet_variant::Variant::new(&metadata, &value);
 
     JsonToVariantTest {
         json: &json,
-        expected: variant,
+        expected: v,
     }
-    .run()
+    .run_without_validation()
 }
 
 #[test]
